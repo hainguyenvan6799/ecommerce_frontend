42b875714812152d1cc5347d95b9c45f55cd3e06
Hai Nguyen <hai.nguyen@kyanon.digital>
2024-02-18 23:03:37 +0700

    fix deploy issue

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,9 +1,9 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://ecommerce-backend-nodejs.herokuapp.com/api";
+    : "https://ecommerce-backend-mern.herokuapp.com/api";
 
 export const serverHost =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000"
-    : "https://ecommerce-backend-nodejs.herokuapp.com";
+    : "https://ecommerce-backend-mern.herokuapp.com";
